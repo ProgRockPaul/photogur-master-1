eb2a491e3edf8dc52435c052645487a9ed5f1dec
Paul Walsh <twentytwentyk@live.com>
2017-02-02 16:30:03 -0500

    add validations to artist, title, url

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ApplicationRecord
+  validates :artist, presence: true
+  validates :title, length: {minimum: 3}
+  validates :title, length: {maximum: 20}
+  validates :url, presence: true, uniqueness: true
+  validates :url, format: 
 end
